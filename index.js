410ffeab46978ac53360434d22e07793a078171e
Mattie43 <ericksenm93@ymail.com>
2020-09-11 18:14:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const newArr = []
+  for (let i = 0; i < names.length; i++) {
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArr
+}
+
+function countDown(num) {
+  while (num > -1) {
+    console.log(num);
+    num --;
+  }
+}
